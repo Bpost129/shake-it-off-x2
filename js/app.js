f8ac167b929503221c7b119c7e17169c13811586
Bpost129 <Bpost129@yahoo.com>
2025-11-22 23:55:01 -0500

    transfer audio and image assets, import functions to app.js, play audio and log random quote data on click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+import { getRandomQuote } from "../data/quotes.js"
+import * as taylorAudio from './audio.js'
+
 const quoteBtn = document.querySelector('#quote-button')
 const cardContainer = document.querySelector('#card-container')
 
 quoteBtn.addEventListener('click', () => {
-  console.log('hey!')
+  taylorAudio.playShakeItOff()
+  console.log(getRandomQuote())
 })
