da76ad938ec516347c2ab686519362e5251b143a
Bpost129 <Bpost129@yahoo.com>
2025-11-22 23:44:18 -0500

    create element references and add event listener to button

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const quoteBtn = document.querySelector('#quote-button')
+const cardContainer = document.querySelector('#card-container')
+
+quoteBtn.addEventListener('click', () => {
+  console.log('hey!')
+})
