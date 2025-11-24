c4a32a58c2c565e9fcc4352b3b0c4421aeabeaf9
Bpost129 <Bpost129@yahoo.com>
2025-11-24 13:53:47 -0500

    add and style delete button on quote cards, implement delete functionality

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const quoteBtn = document.querySelector('#quote-button')
 const cardContainer = document.querySelector('#card-container')
 
 quoteBtn.addEventListener('click', createQuote)
+cardContainer.addEventListener('click', deleteQuote)
 
 function createQuote() {
   taylorAudio.playShakeItOff()
@@ -17,18 +18,30 @@ function createQuote() {
 
 function render() {
   cardContainer.innerHTML = ''
-  quotes.forEach(quote => {
-    appendQuote(quote)
+  quotes.forEach((quote, idx) => {
+    appendQuote(quote, idx)
   })
 }
 
-function appendQuote(quote) {
+function appendQuote(quote, idx) {
   let quoteCard = document.createElement('div')
   quoteCard.className = `card ${quote.isTaylor ? 'taylor' : 'hater'}`
   quoteCard.innerHTML = 
   `<div>
     <p>${quote.text}</p>
     <p>${quote.isTaylor ? '-- T Swift' : '-- Hater Tot'}</p>
-  </div>`
+  </div>
+  <footer>
+    <button class='delete-btn' id='delete-btn-${idx}'>X</button>
+  </footer>`
   cardContainer.appendChild(quoteCard)
+}
+
+function deleteQuote(e) {
+  if(e.target.className = 'delete-btn') {
+    const idx = e.target.id.replace('delete-btn-', '')
+    quotes.splice(idx, 1)
+    taylorAudio.playNoNiceThings()
+    render()
+  }
 }
